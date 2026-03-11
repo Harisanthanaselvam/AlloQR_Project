550d173aeb29cbc24c14a21b5be2bb2c3a8147fe
Harisanthanaselvam <152364843+Harisanthanaselvam@users.noreply.github.com>
2026-03-11 09:03:04 +0530

    Update patients.py

--- backend/routes/patients.py
+++ backend/routes/patients.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session, selectinload
 
+
 from database import get_db
 from models import Patient
 from schemas import PatientCreate, PatientDetail, PatientOut, RegisterPatientResponse
